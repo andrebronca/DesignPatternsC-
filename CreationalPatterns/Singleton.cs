b05bf4d2e2dfdbbcf9c53e3303fe1bde17c26ed1
andre.bronca <bronca.andre@gmail.com>
2025-04-07 09:59:50 -0300

    Padrão criacional - Singleton v1

---
+++ CreationalPatterns/Singleton.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPatternsCSharp.CreationalPatterns;
+
+public sealed class Singleton
+{
+	#region Singleton implementation using static constructor
+
+	private static readonly Singleton Instance;
+	private static int TotalInstances;
+
+	/*
+	 * Private constructior is used to prevent creation of instances with the 'new' keyword outside this class.
+	 */
+	private Singleton()
+	{
+		Console.WriteLine("--Private constructor is called.");
+		Console.WriteLine("--Exit now from private constructor.");
+	}
+
+	/*
+	 * A static constructor is used for the following purposes:
+	 * 1. To initialize any static data
+	 * 2. To perform a specific action only once
+	 * 
+	 * The static constructor will be called automatically before:
+	 * i. You create the first instance; or
+	 * ii. You refer to any static members in your code.
+	 */
+
+	static Singleton()
+	{
+		Console.WriteLine("-Static constructor is called.");
+		Instance = new Singleton();
+		TotalInstances++;
+		Console.WriteLine($"-Singleton instance is created. Number of instances:{TotalInstances}");
+		Console.WriteLine("-Exit from static constructor.");
+	}
+
+	public static Singleton GetInstance => Instance;
+	#endregion
+
+	/* The following line is used to discuss the drawback of the approach. */
+	public static int MyInt = 25;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatternsCSharp.CreationalPatterns;
+
+public class Program
+{
+	public static void Main()
+	{
+		DemonstrationSingletonPattern();
+	}
+
+	private static void DemonstrationSingletonPattern()
+	{
+		Console.WriteLine("***Singleton Pattern Demonstration.***\n");
+		/* The following line is used to discuss the drawback of the approach. */
+		//Console.WriteLine($"The value of MyInt is: {Singleton.MyInt}");
+		// Private Constructor.So, you cannot use the 'new' keyword.
+		//Singleton s = new Singleton(); // error
+		Console.WriteLine("Trying to get a Singleton instance, called firstInstance.");
+		Singleton firstInstance = Singleton.GetInstance;
+		Console.WriteLine("Trying to get another Singleton instance, called secondInstance.");
+		Singleton secondInstance = Singleton.GetInstance;
+
+		if (firstInstance.Equals(secondInstance))
+		{
+			Console.WriteLine("The firstIntance and secondInstance are the same.");
+		}
+		else
+		{
+			Console.WriteLine("Different instances exist.");
+		}
+		Console.ReadKey();
+	}
+}
