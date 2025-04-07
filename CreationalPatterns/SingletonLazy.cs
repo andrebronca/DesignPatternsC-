dd1bac2c310c018125b4e6867aee672d1fa72f23
andre.bronca <bronca.andre@gmail.com>
2025-04-07 10:19:28 -0300

    Lazy. Cria o objeto somente quando é necessário.

---
+++ CreationalPatterns/SingletonLazy.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatternsCSharp.CreationalPatterns;
+
+public sealed class SingletonLazy
+{
+	// custom delegate
+	delegate SingletonLazy SingletonDelegateWithNoParameter();
+	static SingletonDelegateWithNoParameter myDel = MakeSingletonInstance;
+
+	static Func<SingletonLazy> myFuncDelegate = MakeSingletonInstance;
+
+	private static readonly Lazy<SingletonLazy> Instance = new
+		Lazy<SingletonLazy>(
+			//myDel() // also ok. or
+			myFuncDelegate()    // or
+								//() => new SingletonLazy(); // using lambda
+			);
+
+	private static SingletonLazy MakeSingletonInstance()
+	{
+		return new SingletonLazy();
+	}
+
+	private SingletonLazy() { }
+
+	public static SingletonLazy GetInstance => Instance.Value;
+}
